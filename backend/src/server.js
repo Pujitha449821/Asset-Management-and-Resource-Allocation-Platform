fdf2dc865666f2d37f3e76d7c2a5acefa15b3977
Pujitha Velamala <poojithav86@gmail.com>
2026-06-12 13:25:28 +0530

    Authentication module completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,20 @@ require("dotenv").config();
 
 const express = require("express");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 connectDB();
 
+app.use(express.json());
+
+console.log("AUTH ROUTES OBJECT:", authRoutes);
+
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
-  res.send("Server Running");
+  res.send("BHAVANA TEST");
 });
 
 const PORT = process.env.PORT || 5000;
